82a871386d67f2792b174d438825528bc36889ff
ConsueloLibre <consuelomariana.acevedo@gmail.com>
2018-01-08 17:45:16 -0300

    Etapa-4, se appendea, el prototipo otorgado en template, y se rescatan las imagenes desde carpeta de img, además de modificar css para cuadrar textos en img

--- js/script-index.js
+++ js/script-index.js
@@ -48,9 +48,23 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
-}
 
+
+	$('.list-recipes').append(
+		"<a class='item-recipe' href='#'>" +
+			"<span class='attribution'>" +
+				"<span class='title-recipe'>" + recipe.title + "</span>" +
+				"<span class='metadata-recipe'>" +
+				 	"<span class='author-recipe'>" + recipe.source.name + "</span>" +
+					"<span class='bookmarks-recipe'>" +
+						"<span class='icon-bookmark'></span>" +
+					"</span>" +
+				"</span>" +
+			"</span>" +
+			"<img src='img/recipes/640x800/" + recipe.name + ".jpg'>" +
+		"</a>"
+	);
+};
 
 
 /*
